8eae3d49926e52d6ec6151af1566b76f7bedf313
Antti Eskelinen <29664167+EskelinenAntti@users.noreply.github.com>
2025-09-06 09:04:45 +0300

    ignore .git folder

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -3,6 +3,11 @@ return {
 	tag = "0.1.8",
 	dependencies = { "nvim-lua/plenary.nvim" },
 	opts = {
+		defaults = {
+			file_ignore_patterns = {
+				".git",
+			},
+		},
 		pickers = {
 			find_files = {
 				hidden = true,
