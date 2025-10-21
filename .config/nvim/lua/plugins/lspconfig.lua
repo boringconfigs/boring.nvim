0c4330107a6d8d4681d6e01fe7153a0d4d509f65
Antti Eskelinen <29664167+EskelinenAntti@users.noreply.github.com>
2025-10-21 12:31:38 +0300

    Replace blink with native lsp autocomplete

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -2,6 +2,15 @@ return {
 	"neovim/nvim-lspconfig",
 	config = function()
 		local language_servers = require("config.languages").language_servers
+
+		local on_attach = function(client, bufnr)
+			vim.lsp.completion.enable(true, client.id, bufnr, { autotrigger = true })
+		end
+
+		for _, server in ipairs(language_servers) do
+			vim.lsp.config(server, { on_attach = on_attach })
+		end
+
 		vim.lsp.enable(language_servers)
 	end,
 }
