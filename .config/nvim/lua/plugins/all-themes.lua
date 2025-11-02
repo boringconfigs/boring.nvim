d44c10fb0f334d57b8e81aa32e6d0b652d071fbf
Antti Eskelinen <29664167+EskelinenAntti@users.noreply.github.com>
2025-11-02 15:08:03 +0200

    fix theme loading issues

--- .config/nvim/lua/plugins/all-themes.lua
+++ .config/nvim/lua/plugins/all-themes.lua
@@ -1,77 +1,59 @@
-local theme = vim.fn.stdpath("config") .. "/lua/plugins/local/theme.lua"
-local omarchytheme = vim.fn.stdpath("config") .. "/lua/plugins/local/omarchy-theme.lua"
-if vim.fn.filereadable(theme) == 1 then
-    -- User has defined their own theme, configure nothing on our behalf
-    return {}
-elseif vim.fn.filereadable(omarchytheme) == 1 then
-    -- Running in Omarcy
-    return {
-      {
-        "ribru17/bamboo.nvim",
-        lazy = true,
-        priority = 1000,
-      },
-      {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        lazy = true,
-        priority = 1000,
-      },
-      {
-        "neanias/everforest-nvim",
-        lazy = true,
-        priority = 1000,
-      },
-      {
-        "kepano/flexoki-neovim",
-        lazy = true,
-        priority = 1000,
-      },
-      {
-        "ellisonleao/gruvbox.nvim",
-        lazy = true,
-        priority = 1000,
-      },
-      {
-        "rebelot/kanagawa.nvim",
-        lazy = true,
-        priority = 1000,
-      },
-      {
-        "tahayvr/matteblack.nvim",
-        lazy = true,
-        priority = 1000,
-      },
-      {
-        "loctvl842/monokai-pro.nvim",
-        lazy = true,
-        priority = 1000,
-      },
-      {
-        "shaunsingh/nord.nvim",
-        lazy = true,
-        priority = 1000,
-      },
-      {
-        "rose-pine/neovim",
-        name = "rose-pine",
-        lazy = true,
-        priority = 1000,
-      },
-      {
-        "folke/tokyonight.nvim",
-        lazy = true,
-        priority = 1000,
-      },
-    }
-else
-    -- Fallback to catppuccin
-    return {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        priority = 1000,
-        config = function()
-            vim.cmd.colorscheme("catppuccin-macchiato")
-        end,
-      }
-end
+return {
+  {
+    "ribru17/bamboo.nvim",
+    lazy = true,
+    priority = 1000,
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = true,
+    priority = 1000,
+  },
+  {
+    "neanias/everforest-nvim",
+    lazy = true,
+    priority = 1000,
+  },
+  {
+    "kepano/flexoki-neovim",
+    lazy = true,
+    priority = 1000,
+  },
+  {
+    "ellisonleao/gruvbox.nvim",
+    lazy = true,
+    priority = 1000,
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    lazy = true,
+    priority = 1000,
+  },
+  {
+    "tahayvr/matteblack.nvim",
+    lazy = true,
+    priority = 1000,
+  },
+  {
+    "loctvl842/monokai-pro.nvim",
+    lazy = true,
+    priority = 1000,
+  },
+  {
+    "shaunsingh/nord.nvim",
+    lazy = true,
+    priority = 1000,
+  },
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    lazy = true,
+    priority = 1000,
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    priority = 1000,
+  },
+}
--- .config/nvim/omarchy/LazyVim/lua/LazyVim/init.lua
+++ .config/nvim/omarchy/LazyVim/lua/LazyVim/init.lua
@@ -4,7 +4,8 @@ M.opts = {}
 
 -- Workaround for Omarchy depending on LazyVim
 function M.setup(opts)
-	if vim.tbl_deep_extend("force", M.opts, opts or {}).colorscheme then
+    M.opts = vim.tbl_deep_extend("force", M.opts, opts or {})
+	if M.opts.colorscheme then
 		vim.schedule(function()
 			local ok, err = pcall(vim.cmd.colorscheme, M.opts.colorscheme)
 			if not ok then
