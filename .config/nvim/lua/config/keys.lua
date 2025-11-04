854c3e972cdc57383b8eb0adc528c0b99f00f773
Antti Eskelinen <29664167+EskelinenAntti@users.noreply.github.com>
2025-11-04 19:57:46 +0200

    add leader leader shortcut

--- .config/nvim/lua/config/keys.lua
+++ .config/nvim/lua/config/keys.lua
@@ -9,6 +9,7 @@ vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
 -- Telescope
 local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader>f", builtin.find_files, { desc = "Telescope find files" })
+vim.keymap.set("n", "<leader><leader>", builtin.find_files, { desc = "Telescope find files" })
 vim.keymap.set("n", "<leader>/", builtin.live_grep, { desc = "Telescope live grep" })
 
 vim.keymap.set("n", "<leader>b", builtin.buffers, { desc = "Telescope buffers" })
