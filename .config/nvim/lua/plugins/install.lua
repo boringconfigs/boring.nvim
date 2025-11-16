0b7af653dd03a587541762429f05e91c8526b7f6
Antti Eskelinen <29664167+EskelinenAntti@users.noreply.github.com>
2025-11-16 14:33:31 +0200

    shorter syntax with theme plugins

--- .config/nvim/lua/plugins/install.lua
+++ .config/nvim/lua/plugins/install.lua
@@ -16,9 +16,45 @@ end
 require("mini.deps").setup({ path = { package = path_package } })
 
 local add = MiniDeps.add
-add({ source = "stevearc/conform.nvim" })
-add({ source = "tpope/vim-surround" })
+add("stevearc/conform.nvim")
+add("tpope/vim-surround")
+add("neovim/nvim-lspconfig")
+add("nvim-treesitter/nvim-treesitter")
 add({ source = "nvim-telescope/telescope.nvim", checkout = "0.1.8", depends = { "nvim-lua/plenary.nvim" } })
-add({ source = "neovim/nvim-lspconfig" })
-add({ source = "nvim-treesitter/nvim-treesitter" })
-add({ source = "catppuccin/nvim", name = "catppuccin" })
+
+add({
+	source = "EskelinenAntti/omarchy-theme-loader.nvim",
+	checkout = "dev",
+	depends = {
+		"ribru17/bamboo.nvim",
+		{ source = "catppuccin/nvim", name = "catppuccin" },
+		"neanias/everforest-nvim",
+		"kepano/flexoki-neovim",
+		"ellisonleao/gruvbox.nvim",
+		"rebelot/kanagawa.nvim",
+		"tahayvr/matteblack.nvim",
+		"shaunsingh/nord.nvim",
+		{ source = "rose-pine/neovim", name = "rose-pine" },
+		"folke/tokyonight.nvim",
+		"EdenEast/nightfox.nvim",
+		"loctvl842/monokai-pro.nvim",
+	},
+})
+
+require("monokai-pro").setup({
+	filter = "ristretto",
+	override = function()
+		return {
+			NonText = { fg = "#948a8b" },
+			MiniIconsGrey = { fg = "#948a8b" },
+			MiniIconsRed = { fg = "#fd6883" },
+			MiniIconsBlue = { fg = "#85dacc" },
+			MiniIconsGreen = { fg = "#adda78" },
+			MiniIconsYellow = { fg = "#f9cc6c" },
+			MiniIconsOrange = { fg = "#f38d70" },
+			MiniIconsPurple = { fg = "#a8a9eb" },
+			MiniIconsAzure = { fg = "#a8a9eb" },
+			MiniIconsCyan = { fg = "#85dacc" },
+		}
+	end,
+})
