791f9912280d5ad173550974aacc7547814f4513
Antti Eskelinen <29664167+EskelinenAntti@users.noreply.github.com>
2025-11-16 22:03:09 +0200

    switch grep to live_grep

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -23,7 +23,7 @@ fzfLua.setup({})
 
 vim.keymap.set("n", "<leader>f", fzfLua.files, { desc = "Telescope find files" })
 vim.keymap.set("n", "<leader><leader>", fzfLua.files, { desc = "Telescope find files" })
-vim.keymap.set("n", "<leader>/", fzfLua.grep, { desc = "Telescope live grep" })
+vim.keymap.set("n", "<leader>/", fzfLua.live_grep, { desc = "Telescope live grep" })
 vim.keymap.set("n", "<leader>b", fzfLua.buffers, { desc = "Telescope buffers" })
 vim.keymap.set("n", "<leader>d", fzfLua.diagnostics_workspace, { desc = "Telescope diagnostics" })
 vim.keymap.set("n", "<leader>r", fzfLua.lsp_references, { desc = "Telescope LSP references" })
