a49f51b19b3f2484766d314e85b1f629bb5f1927
Antti Eskelinen <29664167+EskelinenAntti@users.noreply.github.com>
2025-11-16 22:03:16 +0200

    simplify lsp setup

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -24,8 +24,9 @@ end
 vim.lsp.config("lua_ls", {
 	settings = {
 		Lua = {
-			runtime = { version = "LuaJIT" },
-			workspace = { library = vim.api.nvim_get_runtime_file("", true) },
+			diagnostics = {
+				globals = { "vim" },
+			},
 		},
 	},
 })
