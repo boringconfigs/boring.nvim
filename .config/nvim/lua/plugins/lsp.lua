a8cd0b8a76c9688fa7ff2614a48e695e72c0ebe1
Antti Eskelinen <29664167+EskelinenAntti@users.noreply.github.com>
2025-11-21 13:14:39 +0200

    remove unnecessary condition

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -23,16 +23,6 @@ end
 
 vim.lsp.config("lua_ls", {
 	on_init = function(client)
-		if client.workspace_folders then
-			local path = client.workspace_folders[1].name
-			if
-				path ~= vim.fn.stdpath("config")
-				and (vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc"))
-			then
-				return
-			end
-		end
-
 		client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
 			runtime = {
 				version = "LuaJIT",
