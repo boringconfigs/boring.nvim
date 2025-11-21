5d7c6693a50029e73c8c51a2575632723040ee44
Antti Eskelinen <29664167+EskelinenAntti@users.noreply.github.com>
2025-11-21 13:23:27 +0200

    refactoring

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -19,20 +19,21 @@ vim.keymap.set("n", "<leader>e", "<cmd>Ex<cr>", { desc = "Open file explorer" })
 vim.keymap.set("t", "<C-w>N", [[<C-\><C-n>]], { desc = "Enter normal mode in terminal" })
 vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
 
+---Build os date objects with given time or current time if not provided.
 ---@param time integer?
 ---@return osdate
 local function osdate(time)
 	return os.date("*t", time) --[[@as osdate]]
 end
 
----Parse date from current buffer name, or return current date.
----@return osdate
+---osdate parsed from current buffer name or nil if it could not be parsed
+---@return osdate|nil
 local function get_buffer_date()
 	local filename = vim.fn.expand("%:t")
 	local y, m, d = filename:match("(%d%d%d%d)%-(%d%d)%-(%d%d)")
 	local date = { year = tonumber(y), month = tonumber(m), day = tonumber(d) }
 	if not (date.year and date.month and date.day) then
-		return osdate()
+		return nil
 	end
 
 	return osdate(os.time(date))
@@ -48,6 +49,10 @@ end
 ---@param offset integer Offset from the current day or note that is currently open. 1 meaning tomorrow, -1 yesterday, for example.
 local function open_daily_at_offset(offset)
 	local base = get_buffer_date()
+	if not base then
+		base = osdate()
+	end
+
 	base.day = base.day + offset
 	open_daily_note(base)
 end
